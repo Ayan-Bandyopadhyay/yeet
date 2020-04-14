4c05f6d4340cc08ce9ec8a42edf4fb0baeb26409
Ayan  Bandyopadhyay <ayanbandyopadhyay@yeet-squad.attlocal.net>
2020-04-14 15:14:31 -0700

    yeet

--- update_dependencies.py
+++ update_dependencies.py
@@ -1,5 +1,7 @@
 import json
 
+import os
+
 from pathlib import Path
 home = str(Path.home())
 
@@ -9,3 +11,11 @@ with open(home + "/files.json") as file:
 	data = json.load(file)
 
 print(data)
+
+
+
+for root, dirs, files in os.walk(home, topdown=True):
+   for name in files:
+      print(os.path.join(root, name))
+   for name in dirs:
+      print(os.path.join(root, name))
