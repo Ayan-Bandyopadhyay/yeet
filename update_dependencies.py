a57a82e4423f75dd633c9712ba839e3df1f9ad18
Ayan  Bandyopadhyay <ayanbandyopadhyay@yeet-squad.attlocal.net>
2020-04-14 14:58:26 -0700

    ye

--- update_dependencies.py
+++ update_dependencies.py
@@ -1,8 +1,11 @@
 import json
 
+from pathlib import Path
+home = str(Path.home())
+
 data = None
 
-with open("$HOME/files.json") as file:
+with open(home + "/files.json") as file:
 	data = json.load(file)
 
 print(data)
