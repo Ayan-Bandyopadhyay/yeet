ac544f5af5fe482cb4d9f92dd53970845029eaec
Ayan  Bandyopadhyay <ayanb9440@gmail.com>
2020-06-02 01:15:34 -0700

    ye

--- gitpython_test.py
+++ gitpython_test.py
@@ -5,6 +5,8 @@ import git
 import dvc.api
 from dvc.repo import Repo
 import sys
+from distutils.dir_util import copy_tree
+import shutil
 
 # ssh_password = sys.argv[1]
 
@@ -14,10 +16,31 @@ shgit = sh.git.bake(_cwd=dir_path)
 
 repo = git.Repo(dir_path)
 
-
+origin = repo.remote('origin')
 
 # print(ssh_password)
 
 
 
-Repo.get('https://github.com/Ayan-Bandyopadhyay/yeet', 'data6/')
+# Repo.get('https://github.com/Ayan-Bandyopadhyay/yeet', 'data6/')
+
+# shutil.rmtree('data6')
+# os.makedirs('data6')
+# with open("data6/copy.txt", "w") as file:
+#     file.write("Your text goes here")
+
+# dvc_repo = Repo()
+# dvc_repo.add('data6')
+# dvc_repo.pull()
+# dvc_repo.push()
+
+
+# repo.index.add('data6.dvc')
+# repo.index.commit('update data6')
+# origin.push()
+
+from dvc.config import Config
+
+config = Config()
+with config.edit(0) as conf:                       
+    conf["remote"]['myremote']['keyfile'] = 'value'
